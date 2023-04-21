cb412ae1ac3faeebba466480ffb9469bc089c279
KaSui <1003753036@qq.com>
2023-04-21 10:11:15 +0800

    upload

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # string_agg
  string_agg for SQL Server 2017 lower
+
+1.解决方案名右键属性调整项目配置的目标平台调整，为你的SQL版本
+
+2.连接符为“,”
+
+```c#
+public void Accumulate(SqlString value)
+{
+    if (value.IsNull)
+    {
+        return;
+    }
+
+    this.intermediateResult.Append(value.Value).Append(',');
+}
+
+```
+
+3.数据库配置
+
+将生成的.dll文件放到好找的地方
+
+依次执行以下SQL命令
+
+```sql
+CREATE ASSEMBLY StringAgg 
+FROM 'D:\MSSQL_String_Agg_Function\string_agg.dll'
+WITH PERMISSION_SET = SAFE;
+
+exec sp_configure 'clr enabled',1
+
+RECONFIGURE WITH OVERRIDE;
+
+CREATE AGGREGATE string_agg(@input nvarchar(4000))
+RETURNS nvarchar(max)
+EXTERNAL NAME [StringAgg].[string_agg];
+```
+
+3.使用
+
+```sql
+select dbo.string_agg(f) from t group by xx
+```
---
+++ string_agg.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Data;
+using Microsoft.SqlServer.Server;
+using System.Data.SqlTypes;
+using System.IO;
+using System.Text;
+
+[Serializable]
+[SqlUserDefinedAggregate(
+    Format.UserDefined, //use clr serialization to serialize the intermediate result
+    IsInvariantToNulls = true, //optimizer property
+    IsInvariantToDuplicates = false, //optimizer property
+    IsInvariantToOrder = false, //optimizer property
+    MaxByteSize = 8000) //maximum size in bytes of persisted value
+]
+public class string_agg : IBinarySerialize
+{
+    /// <summary>
+    /// The variable that holds the intermediate result of the concatenation
+    /// </summary>
+    private StringBuilder intermediateResult;
+
+    /// <summary>
+    /// Initialize the internal data structures
+    /// </summary>
+    public void Init()
+    {
+        this.intermediateResult = new StringBuilder();
+    }
+
+    /// <summary>
+    /// Accumulate the next value, not if the value is null
+    /// </summary>
+    /// <param name="value"></param>
+    public void Accumulate(SqlString value)
+    {
+        if (value.IsNull)
+        {
+            return;
+        }
+
+        this.intermediateResult.Append(value.Value).Append(',');
+    }
+
+    /// <summary>
+    /// Merge the partially computed aggregate with this aggregate.
+    /// </summary>
+    /// <param name="other"></param>
+    public void Merge(string_agg other)
+    {
+        this.intermediateResult.Append(other.intermediateResult);
+    }
+
+    /// <summary>
+    /// Called at the end of aggregation, to return the results of the aggregation.
+    /// </summary>
+    /// <returns></returns>
+    public SqlString Terminate()
+    {
+        string output = string.Empty;
+        //delete the trailing comma, if any
+        if (this.intermediateResult != null
+            && this.intermediateResult.Length > 0)
+        {
+            output = this.intermediateResult.ToString(0, this.intermediateResult.Length - 1);
+        }
+
+        return new SqlString(output);
+    }
+
+    public void Read(BinaryReader r)
+    {
+        intermediateResult = new StringBuilder(r.ReadString());
+    }
+
+    public void Write(BinaryWriter w)
+    {
+        w.Write(this.intermediateResult.ToString());
+    }
+}
